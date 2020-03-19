8c3cc76411026a94f661325457b0178d259cca9c
zhangzhitan <1305864551@qq.com>
2020-03-19 16:51:00 +0800

    新增README内容

--- sql_scripts/add_feature1.sql
+++ sql_scripts/add_feature1.sql
@@ -1,39 +1,69 @@
-update Tablename set lastarrtime=thisdeptime where lastarrtime='start'
-update Tablename set thisdeptime=lastarrtime where thisdeptime='end'
-update Tablename set thisarrtime=nextdeptime where thisarrtime='start'
-update Tablename set nextdeptime=thisarrtime where nextdeptime='end'
+create procedure add_feature (@table_name varchar(30))
+as
+begin
+declare @sql nvarchar(max)
+
+set @sql = 'update '+ @table_name + ' set lastarrtime=thisdeptime where lastarrtime=''start'''
+EXEC(@sql)
+set @sql = 'update '+ @table_name +' set thisdeptime=lastarrtime where thisdeptime=''end'''
+EXEC(@sql)
+set @sql = 'update '+ @table_name +' set thisarrtime=nextdeptime where thisarrtime=''start'''
+EXEC(@sql)
+set @sql = 'update '+ @table_name +' set nextdeptime=thisarrtime where nextdeptime=''end'''
+EXEC(@sql)
 
 
-alter table Tablename alter column lastarrtime datetime
-alter table Tablename alter column thisdeptime datetime
-alter table Tablename alter column thisarrtime datetime
-alter table Tablename alter column nextdeptime datetime
-alter table Tablename alter column delaydetail smallint
-alter table Tablename alter column rownum tinyint
-alter table Tablename alter column allrow tinyint
+set @sql ='alter table ' + @table_name + ' alter column lastarrtime datetime'
+EXEC(@sql)
+set @sql ='alter table ' + @table_name + ' alter column thisdeptime datetime'
+EXEC(@sql)
+set @sql ='alter table ' + @table_name + ' alter column thisarrtime datetime'
+EXEC(@sql)
+set @sql ='alter table ' + @table_name + ' alter column nextdeptime datetime'
+EXEC(@sql)
+set @sql ='alter table ' + @table_name + ' alter column delaydetail smallint'
+EXEC(@sql)
+set @sql ='alter table ' + @table_name + ' alter column rownum tinyint'
+EXEC(@sql)
+set @sql ='alter table ' + @table_name + ' alter column allrow tinyint'
+EXEC(@sql)
 
-alter table Tablename add pair varchar(30)
-update Tablename set pair=thisdepcode+thisarrcode
+set @sql ='alter table ' + @table_name + ' add pair varchar(30)'
+EXEC(@sql)
+set @sql ='alter table ' + @table_name + ' add accumulative float'
+EXEC(@sql)
+set @sql ='alter table ' + @table_name + ' add t_month tinyint'
+EXEC(@sql)
+set @sql ='alter table ' + @table_name + ' add t_quart tinyint'
+EXEC(@sql)
+set @sql ='alter table ' + @table_name + ' add t_week tinyint'
+EXEC(@sql)
+set @sql ='alter table ' + @table_name + ' add t_hour tinyint'
+EXEC(@sql)
+set @sql ='alter table ' + @table_name + ' add t_period smallint'
+EXEC(@sql)
+set @sql ='alter table ' + @table_name + ' add t_type char(1)'
+EXEC(@sql)
 
-alter table Tablename add accumulative float
-update Tablename set accumulative = 1.0*rownum/allrow
 
+set @sql ='update ' + @table_name + ' set pair=thisdepcode+thisarrcode'
+EXEC(@sql)
+set @sql ='update ' + @table_name + ' set accumulative = 1.0*rownum/allrow'
+EXEC(@sql)
+set @sql ='update ' + @table_name + ' set t_month = DATEPART(mm, thisdeptime)'
+EXEC(@sql)
+set @sql ='update ' + @table_name + ' set t_quart = DATEPART(qq, thisdeptime)'
+EXEC(@sql)
+set @sql ='update ' + @table_name + ' set t_week = DATEPART(w, thisdeptime)'
+EXEC(@sql)
+set @sql ='update ' + @table_name + ' set t_hour = DATEPART(hh, thisdeptime)'
+EXEC(@sql)
+set @sql ='update ' + @table_name + ' set t_period = DATEDIFF(mi, thisdeptime, thisarrtime)'
+EXEC(@sql)
+set @sql ='update ' + @table_name + ' set t_type = SUBSTRING(realtrainnum, 1, 1)'
+EXEC(@sql)
+set @sql ='update ' + @table_name + ' set t_type = ''P'' WHERE t_type like ''[0-9]'' '
+EXEC(@sql)
 
-alter table Tablename add t_month tinyint
-update Tablename set t_month = DATEPART(mm, thisdeptime)
+end
 
-alter table Tablename add t_quart tinyint
-update Tablename set t_quart = DATEPART(qq, thisdeptime)
-
-alter table Tablename add t_week tinyint
-update Tablename set t_week = DATEPART(w, thisdeptime)
-
-alter table Tablename add t_hour tinyint
-update Tablename set t_hour = DATEPART(hh, thisdeptime)
-
-alter table Tablename add period smallint
-update Tablename set period = DATEDIFF(mi, thisdeptime, thisarrtime)
-
-alter table Tablename add t_type char(1)
-update Tablename set t_type = SUBSTRING(realtrainnum, 1, 1)
-update Tablename set t_type = 'P' WHERE t_type like '[0-9]'
